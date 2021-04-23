1115f78f59b5b7dc061a49cbdd59f7f74c602857
JaySaints <54243713+JaySaints@users.noreply.github.com>
2021-04-23 09:06:35 -0300

    Mostra dados das de todas as tabelas

---
+++ ListDatasAllTables.sql
@@ -0,0 +1,16 @@
+-- Seleciosa o Banco de dados a ser usado
+USE DB_BlockChain
+GO
+-- Lista o conteudo de todas as tabelas do banco de dados DB_BlockChain
+SELECT * FROM Corretora
+GO
+SELECT * FROM Moeda
+GO
+SELECT * FROM ParesMoedas
+GO
+SELECT * FROM Cliente
+GO
+SELECT * FROM Carteira
+GO
+SELECT * FROM ItemCarteira
+GO
