9be80d8fdfb931852ff598e9752299f157e604ca
JaySaints <54243713+JaySaints@users.noreply.github.com>
2021-04-26 20:42:21 -0300

    Função para consultar o valor cotação
    Consulta a cotação da moeda desejada com base nos parametros, codigo moeda cotação e codigo moeda base

---
+++ CotacaoAtual.sql
@@ -0,0 +1,20 @@
+-- Seleciosa o Banco de dados a ser usado
+USE DB_BlockChain
+GO
+-- Cria função para pegar a cotação atual da moeda desejada na tabela ParesMoedas. É passado dois parametros: 
+-- 1° é passado o código da moeda base
+-- 2° é passado o código da moeda principal do cliente
+CREATE FUNCTION CotacaoAtual(@CodMoedaBase VARCHAR(3), @CodMoedaPrincipal VARCHAR(3))
+RETURNS FLOAT
+AS 
+BEGIN
+	DECLARE @ValorCotacao FLOAT;
+	SELECT @ValorCotacao = Valor FROM ParesMoedas 
+		WHERE CodigoMoedaBase = @CodMoedaBase 
+			AND CodigoMoedaCotacao = @CodMoedaPrincipal
+	RETURN @ValorCotacao
+END
+
+-- Chamada da função para obter o valor da moeda Ethereum
+--SELECT dbo.CotacaoAtual('ETH', 'USD')
+
