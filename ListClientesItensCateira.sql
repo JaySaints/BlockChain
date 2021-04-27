8c345d1b12fbdb02ecbe646cf5dd5ffb01d0a023
JaySaints <54243713+JaySaints@users.noreply.github.com>
2021-04-27 08:46:56 -0300

    Inserido Schema

--- ListClientesItensCateira.sql
+++ ListClientesItensCateira.sql
@@ -3,7 +3,8 @@ USE DB_BlockChain
 GO
 -- View para mostrar o Codigo do Cliente, Nome dos Clientes, Codigo das moesdas que o cliente possui
 -- quantidade de moedas e o valor (valor= quantidade da moeda vezes a cotação daquela respctiva moeda).
-CREATE VIEW ListaItensCliente AS
+CREATE VIEW sch_blockchain.ListaItensCliente 
+AS
 SELECT Cliente.CodigoCliente , Cliente.Nome, ItemCarteira.CodigoMoeda, ItemCarteira.Quantidade, 
 	(dbo.CotacaoAtual(ItemCarteira.CodigoMoeda, Cliente.MoedaPrincipal) * ItemCarteira.Quantidade) AS Valor  
 	FROM Cliente
