7ac1b765f98c67568fa7a49059a8ffb335ca8237
JaySaints <54243713+JaySaints@users.noreply.github.com>
2021-04-28 22:48:42 -0300

    Rename 9-ViewListaClientesItensCateira.sql to 10-ViewListaClientesItensCateira.sql

--- 10-ViewListaClientesItensCateira.sql
+++ 10-ViewListaClientesItensCateira.sql
@@ -2,7 +2,7 @@
 USE DB_BlockChain
 GO
 -- View para mostrar o Codigo do Cliente, Nome dos Clientes, Codigo das moesdas que o cliente possui
--- quantidade de moedas e o valor (valor= quantidade da moeda vezes a cotação daquela respctiva moeda).
+-- quantidade de moedas e o valor (valor= quantidade da moeda vezes a cotaÃ§Ã£o daquela respctiva moeda).
 CREATE VIEW sch_blockchain.ListaItensCliente AS
 SELECT Cliente.CodigoCliente , Cliente.Nome, ItemCarteira.CodigoMoeda, ItemCarteira.Quantidade, 
 	(sch_blockchain.CotacaoAtual(ItemCarteira.CodigoMoeda, Cliente.MoedaPrincipal) * ItemCarteira.Quantidade) AS Valor  
@@ -12,7 +12,7 @@ SELECT Cliente.CodigoCliente , Cliente.Nome, ItemCarteira.CodigoMoeda, ItemCarte
 	INNER JOIN
 		sch_blockchain.ItemCarteira ON ItemCarteira.Endereco = Carteira.Endereco	
 	
--- Chama um elemento da view, passando como parametro de seleção o codigo do cliente
+-- Chama um elemento da view, passando como parametro de seleÃ§Ã£o o codigo do cliente
 -- SELECT * FROM sch_blockchain.ListaItensCliente WHERE CodigoCliente = 1
 
 -- Chama todos os elementos da tabela e ordena pelo Codigo do cliente
